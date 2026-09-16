10aff52fa406706130076c953c685995bdf3fd7a
aomsin0135-art <aomsin0135@gmail.com>
2026-09-16 11:06:26 +0700

    Update server.ts

--- server.ts
+++ server.ts
@@ -11,7 +11,7 @@ dotenv.config();
 dns.setDefaultResultOrder("ipv4first");
 
 const app = express();
-const PORT = 3000;
+const PORT = Number(process.env.PORT) || 3000;
 
 app.use(express.json());
 
